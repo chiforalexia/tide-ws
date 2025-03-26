823c91234e97bf7541708f546716f9aebbbb9f55
THeerze <teun.heerze@student.uva.nl>
2025-03-26 17:57:11 +0100

    added social icons like on SURF.nl

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -17,11 +17,35 @@
                     Guiding institutions through successful technology adoption with collaborative tools and expert
                     knowledge.
                 </p>
-                <div class="flex space-x-4 mt-4 text-gray-700">
-                    <a href="#"><i class="fab fa-twitter"></i></a>
-                    <a href="#"><i class="fab fa-linkedin"></i></a>
-                    <a href="#"><i class="fab fa-facebook"></i></a>
+                <div class="flex items-center gap-4 mt-4">
+                    <!-- LinkedIn -->
+                    <a href="#" aria-label="LinkedIn">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-600 hover:text-blue-700"
+                            fill="currentColor" viewBox="0 0 24 24">
+                            <path
+                                d="M4.98 3.5a2.5 2.5 0 11-.01 5.01 2.5 2.5 0 01.01-5.01zM3 8.98h3.96V21H3V8.98zM8.98 8.98h3.8v1.63h.05c.53-1 1.83-2.06 3.77-2.06 4.03 0 4.77 2.65 4.77 6.09V21h-3.97v-5.59c0-1.33-.03-3.04-1.86-3.04-1.87 0-2.16 1.46-2.16 2.95V21h-3.96V8.98z" />
+                        </svg>
+                    </a>
+
+                    <!-- YouTube -->
+                    <a href="#" aria-label="YouTube">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-600 hover:text-red-600"
+                            viewBox="0 0 24 24" fill="currentColor">
+                            <path
+                                d="M23.5 6.2s-.2-1.6-.8-2.3c-.8-.9-1.6-1-2-1.1C17.9 2.5 12 2.5 12 2.5h0s-5.9 0-8.7.3c-.4.1-1.2.2-2 1.1C.7 4.6.5 6.2.5 6.2S0 8.1 0 10v4c0 1.9.5 3.8.5 3.8s.2 1.6.8 2.3c.8.9 1.9.9 2.4 1 1.7.2 7.3.3 8.3.3s5.9 0 8.7-.3c.4-.1 1.2-.2 2-1.1.6-.7.8-2.3.8-2.3s.5-1.9.5-3.8v-4c0-1.9-.5-3.8-.5-3.8zM9.75 15.02v-6l6 3-6 3z" />
+                        </svg>
+                    </a>
+
+                    <!-- Mastodon -->
+                    <a href="#" aria-label="Mastodon">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-600 hover:text-indigo-500"
+                            viewBox="0 0 24 24" fill="currentColor">
+                            <path
+                                d="M21.43 11.07c-.23 1.73-2.05 3.66-4.14 4.01-1.1.18-2.15.35-3.3.34-1.64-.02-2.64-.4-2.64-.4v2.6s.02 1.1-1.38 1.29c-1.55.2-2.77-.06-3.06-.18-.23-.1-.36-.32-.36-.57v-2.23s-3.53.77-4.48-1.58C1.23 12.47 1 9.3 1 6.2V6.1C1 3.36 3.07 1.3 5.8 1.1c0 0 1.94-.14 5.84-.14s5.84.14 5.84.14c2.74.2 4.8 2.26 4.8 5v.1c0 1.53.05 3.06-.25 4.46zM17 8.5h-2v3.5c0 .49-.4.88-.88.88h-.25c-.48 0-.87-.4-.87-.88V8.5h-2v3.5c0 .49-.4.88-.88.88h-.25c-.48 0-.87-.4-.87-.88V8.5H6v3.75c0 .49.12 1.02.37 1.4.45.7 1.28 1.13 2.48 1.17 2.3.07 2.88-.6 2.88-.6s.57.65 2.88.6c1.2-.04 2.03-.47 2.48-1.17.25-.38.37-.9.37-1.4V8.5z" />
+                        </svg>
+                    </a>
                 </div>
+
             </div>
 
             <!-- Contact Info -->
