f56dd766d38acc02de5be41063f68d564c8a65de
alicja-a-d <alicja.dorobis@student.uva.nl>
2025-04-10 12:55:51 +0200

    added report download button

--- src/components/Website_introduction.vue
+++ src/components/Website_introduction.vue
@@ -62,22 +62,32 @@ export default {
           </div>
         </div>
         
-        <div class="image-container">
-          <transition name="fade" mode="out-in">
-            <img 
-              :key="currentImageIndex"
-              :src="images[currentImageIndex]" 
-              :alt="`Slide ${currentImageIndex + 1}`"
-              class="report-image" 
-            />
-          </transition>
+        <div class="image-wrapper flex flex-col items-center space-y-4 mt-6">
+          <div class="image-container">
+            <transition name="fade" mode="out-in">
+              <img 
+                :key="currentImageIndex"
+                :src="images[currentImageIndex]" 
+                :alt="`Slide ${currentImageIndex + 1}`"
+                class="report-image" 
+              />
+            </transition>
+          </div>
+          <div>
+            <a 
+              href="https://www.surf.nl/en/tech-trends"
+              class="inline-block bg-green-100 text-green-800 px-3 py-1.5 rounded-full shadow-md transition-all duration-300 hover:bg-white hover:bg-green-200 transition"
+            >
+              Download Full Report
+            </a>
+          </div>
         </div>
       </div>
   
       <!-- Bubble -->
       <div class="transition-all duration-700 delay-500 opacity-100">
         <a href="#chapters" class="inline-flex items-center animate-bounce bg-blue-100 text-blue-800 px-3 py-1.5 rounded-full text-sm font-medium hover:bg-green-200 transition">
-          <span class="mr-2">We're here to translate strategic vision into measurable impact.</span>
+          <span class="mr-2">Translate your vision into real impact</span>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
               class="w-4 h-4 animate-bounce">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
