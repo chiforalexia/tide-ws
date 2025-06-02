32725b61c8ef5135eba9467bfbdf55c8ab5aff65
ellasartory <ella.sartory@student.uva.nl>
2025-06-02 11:51:47 +0200

    Insert World Map

--- src/components/succes_stories/WorldMap.vue
+++ src/components/succes_stories/WorldMap.vue
@@ -1,4 +1,17 @@
 <template>
+    <!-- Title section -->
+    <section class="py-16 px-4 max-w-7xl mx-auto">
+      <div class="text-center mb-12">
+        <div class="bubble">
+          Innovation Stats
+        </div>
+        <h2 class="text-4xl font-bold mb-4">Innovation around the World </h2>
+        <!-- Subtitle section -->
+        <p class="text-gray-600 text-xl max-w-2xl mx-auto">
+          Discover and compare innovation stats form each country.
+        </p>
+      </div>
+    </section>
     <div id="map"
         class="hidden sm:block max-w-5xl mx-auto h-[600px] border-[0.5vw] border-[#385e8a] p-[1vw] m-[3vw] z-[2]"></div>
 </template>
--- src/pages/SuccessStoriesPage.vue
+++ src/pages/SuccessStoriesPage.vue
@@ -10,8 +10,14 @@
 
       <section id="features">
         <!-- Add your features section content here -->
+        <WorldMap />
+      </section>
+
+      <section id="features">
+        <!-- Add your features section content here -->
         <Roadmap-template />
       </section>
+
     </div>
   </div>
 </template>
@@ -22,6 +28,7 @@ import Navbar from "../components/common/Navbar.vue";
 import Footer from "../components/common/Footer.vue";
 import SuccessStories from "../components/succes_stories/SuccessStories.vue";
 import RoadmapTemplate from "../components/succes_stories/RoadmapTemplate.vue";
+import WorldMap from "../components/succes_stories/WorldMap.vue";
 
 export default {
   name: "SuccessStoriesPage",
@@ -31,7 +38,8 @@ export default {
     Footer,
     SuccessStories,
     RoadmapTemplate,
-  },
+    WorldMap,
+  }
 };
 </script>
 
